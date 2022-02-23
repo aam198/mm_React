ccd01d3a7fffd45ed2c7a0f1bdc54d78326e056f
aam198 <aam198@psu.edu>
2022-02-23 15:21:44 -0500

    Auth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Auth } from 'aws-amplify'
+import '@aws-amplify/ui-react/styles.css'
 
-function App() {
+function checkUser(){
+  Auth.currentAuthenticatedUser()
+    .then(user => console.log({ user }))
+    .catch(err => console.log(err))
+}
+
+function signOut(){
+  Auth.signOut()
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+}
+
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Hello from V2</h1>
-        </p>
+          <h1>Hello from Auth Version</h1>
+      
+        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+        <button onClick={checkUser}>Check User</button>
+        <button onClick={signOut}>Sign Out</button>
         
       </header>
     </div>
